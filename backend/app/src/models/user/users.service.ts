74715497f374169a9dbf65f8c63c83a02b9dfc11
DomiVesalius <mmillion448@gmail.com>
2023-02-03 19:22:23 -0500

    Implemented password comparison function for login feature

--- backend/app/src/models/user/users.service.ts
+++ backend/app/src/models/user/users.service.ts
@@ -1,4 +1,5 @@
 import UserModel, { IUserModel } from './user.model';
+import bcrypt from 'bcrypt';
 
 export class UsersService {
     public static async getByEmail(email: string): Promise<IUserModel | null> {
@@ -15,5 +16,20 @@ export class UsersService {
         } catch (e) {
             return null;
         }
+    }
+
+    /**
+     * Checks if the given plaintext password matches the hash+salt of the user
+     * with the given email.
+     * @param email identifies the user being compared to
+     * @param password a plaintext password
+     * @return false if the user does not exist or password does not match the hash. true otherwise
+     */
+    public static async comparePassword(email: string, password: string): Promise<boolean> {
+        const user = await this.getByEmail(email);
+
+        if (!user) return false;
+
+        return bcrypt.compare(password, user.password);
     }
 }

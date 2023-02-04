c85f4e11ef4e8003491774fe4dd4d00c33320ec5
Eric Ryu <ericryu19@gmail.com>
2023-02-04 05:59:15 -0500

    styled search bar using mui

--- frontend/app/src/components/SearchBar/SearchBar.tsx
+++ frontend/app/src/components/SearchBar/SearchBar.tsx
@@ -1,24 +1,63 @@
 import React from 'react';
+import { alpha, createTheme, InputBase, styled } from '@mui/material';
 
 interface SearchProps {
-    /** Background color */
-    backgroundColor?: string;
+    /** Color of Search Bar when hovering */
+    hoverColor: string;
 
-    /** Size of Search Bar */
-    size?: 'small' | 'medium' | 'large';
+    /** Color of Search Bar (default) */
+    defaultColor: string;
 
     /** Placeholder text in search */
     placeHolder: string;
 }
 
+/* styled('div') creates custom divs with start and end tag of the constant name */
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
 export const SearchBar = ({
+    hoverColor,
+    defaultColor,
     placeHolder,
     ...props
 }: SearchProps) => {
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(defaultColor ? defaultColor : theme.palette.primary.main, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(hoverColor ? hoverColor : theme.palette.primary.main, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    }));
     return (
-        <form id="form">
-            <input type="search" id="courseSearch" placeholder={placeHolder} />
-            <button>Search</button>
-        </form>
+        <Search>
+            {/*<SearchIconWrapper>*/}
+            {/*    <SearchIcon />*/}
+            {/*</SearchIconWrapper>*/}
+            <StyledInputBase
+                placeholder={placeHolder}
+                inputProps={{ 'aria-label': 'search' }}
+            />
+        </Search>
     );
 };

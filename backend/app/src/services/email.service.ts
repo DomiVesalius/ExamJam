26da5a2907500b3b7cfe5d586a2ca423d2437df8
DomiVesalius <mmillion448@gmail.com>
2023-02-05 15:07:32 -0500

    Created service for sending emails

---
+++ backend/app/src/services/email.service.ts
@@ -0,0 +1,40 @@
+import nodemailer, { SentMessageInfo, SendMailOptions } from 'nodemailer';
+import jwt from 'jsonwebtoken';
+
+import { IUserModel } from '../models/user/user.model';
+import SERVER_CONFIG from '../config/server.config';
+import logger from '../utils/logger.util';
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.NO_REPLY_EMAIL,
+        pass: process.env.NO_REPLY_EMAIL_PWD
+    }
+});
+
+export class EmailService {
+    public static async sendVerificationEmail(user: IUserModel): Promise<Error | SentMessageInfo> {
+        const token = jwt.sign({ data: user.email }, SERVER_CONFIG.verifyEmailJwtSecret, {
+            expiresIn: '1h'
+        });
+
+        const mailOptions: SendMailOptions = {
+            from: process.env.NO_REPLY_EMAIL,
+            to: user.email,
+            subject: 'Verify your ExamJam account',
+            text:
+                'Click the link below to verify your email and activate your account:' +
+                `\nlocalhost/api/user/verify-email?token=${token}\nThe token will expire in 1 hour.`
+        };
+
+        try {
+            const messageInfo = await transporter.sendMail(mailOptions);
+            logger.info(`Verification email sent to ${user.email}`);
+            return messageInfo;
+        } catch (e) {
+            logger.info(`Failed to send verification email to ${mailOptions.to}`, e);
+            return null;
+        }
+    }
+}

57e54c3cf01000346524afa096152d6a058e1333
DomiVesalius <mmillion448@gmail.com>
2023-02-05 13:15:21 -0500

    Configured passport and session middleware for authentication system

---
+++ backend/app/src/middlewares/passport.middleware.ts
@@ -0,0 +1,48 @@
+import passport from 'passport';
+import PassportLocal, { IVerifyOptions } from 'passport-local';
+
+import { UsersService } from '../models/user/users.service';
+import { IUserModel } from '../models/user/user.model';
+
+type DoneFunction = (error: any, user?: Express.User | false, options?: IVerifyOptions) => void;
+
+passport.use(
+    new PassportLocal.Strategy(
+        { usernameField: 'email' },
+        async (email: string, password: string, done: DoneFunction) => {
+            try {
+                if (await UsersService.comparePassword(email, password)) {
+                    // At this point, a user exists because comparePassword returned true
+                    const user: IUserModel = <IUserModel>await UsersService.getByEmail(email);
+
+                    // Only check if the user account is activated in non-development environment
+                    // to make it easier to for manual testing. Check will occur in 'production' and
+                    // 'test' environments
+                    if (process.env.NODE_ENV !== 'development') {
+                        if (user.active) {
+                            done(null, user);
+                        } else {
+                            done(null, false);
+                        }
+                    }
+                } else {
+                    // Since no user was found & there was no error, pass user as false to show
+                    // unsuccessful auth (invalid credentials)
+                    done(null, false);
+                }
+            } catch (e) {
+                done(e);
+            }
+        }
+    )
+);
+
+passport.serializeUser((user: Express.User, done: DoneFunction) => done(null, user));
+
+passport.deserializeUser((user: Express.User, done) => done(null, user));
+
+enum PassportStrategies {
+    local = 'local'
+}
+
+export default PassportStrategies;

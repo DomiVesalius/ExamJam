f76595a430bd1ca922072ecae8791be695817f14
DomiVesalius <mmillion448@gmail.com>
2023-02-05 15:06:16 -0500

    Added mailing service credentials & secret key to env file

--- backend/app/src/config/server.config.ts
+++ backend/app/src/config/server.config.ts
@@ -3,17 +3,20 @@ type ServerConfig = {
     host: string;
     url: string;
     cookieSecret: string;
+    verifyEmailJwtSecret: string;
 };
 
 const host = process.env.SERVER_HOST || 'localhost';
 const port = parseInt(process.env.SERVER_PORT || '8080');
 const url = `${host}:${port}/api`;
 const cookieSecret = process.env.COOKIE_SECRET || 'Secret';
+const verifyEmailJwtSecret = process.env.VERIFY_EMAIL_JWT_SECRET || 'Verify Email Secret Key';
 
 const SERVER_CONFIG: ServerConfig = {
     host,
     port,
     url,
+    verifyEmailJwtSecret,
     cookieSecret
 };
 

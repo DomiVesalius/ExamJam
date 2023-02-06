a7d36f076f170f44e8b784cab7de720c0cbb6547
Eric Ryu <ericryu19@gmail.com>
2023-02-06 12:56:12 -0500

    add autocompletion and search icon to search bar

--- frontend/app/src/components/SearchBar/SearchBar.tsx
+++ frontend/app/src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { alpha, createTheme, InputBase, styled } from '@mui/material';
+import { alpha, createTheme, InputBase, styled, Autocomplete } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 interface SearchProps {
     /** Color of Search Bar when hovering */
@@ -28,6 +29,16 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     },
 }));
 
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
 export const SearchBar = ({
     hoverColor,
     defaultColor,
@@ -49,12 +60,31 @@ export const SearchBar = ({
             width: 'auto',
         },
     }));
+
+    const top100Films = [
+        { title: "The Shawshank Redemption", year: 1994 },
+        { title: "The Godfather", year: 1972 },
+        { title: "The Godfather: Part II", year: 1974 },
+        { title: "The Dark Knight", year: 2008 },
+        { title: "12 Angry Men", year: 1957 }
+    ];
+
     return (
-        <Search>
-            <StyledInputBase
-                placeholder={placeHolder}
-                inputProps={{ 'aria-label': 'search' }}
-            />
-        </Search>
+        <Autocomplete renderInput={(params) => {
+            const { InputLabelProps, InputProps, ...rest } = params;
+            return (
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        {...params.InputProps}
+                        {...rest}
+                        placeholder={placeHolder}
+                    />
+                </Search>
+            );
+        }} options={top100Films} getOptionLabel={ (option) => option.title }
+        />
     );
 };

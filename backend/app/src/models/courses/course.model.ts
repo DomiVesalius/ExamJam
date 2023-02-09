fa497cbcea9946538cde32c6d4bb5e9a9e9243f6
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2023-02-08 22:41:07 -0500

    added exam model and changed dbconfig name to match local db

--- backend/app/src/models/courses/course.model.ts
+++ backend/app/src/models/courses/course.model.ts
@@ -4,17 +4,22 @@ export interface ICourse {
     courseCode: string;
     title: string;
     description: string;
-    programArea: string;
+    programArea: Array<string>;
+    campuses: Array<string>;
 }
 
 export interface ICourseModel extends ICourse, Document {}
+
+// programArea: string[]
+// campus: string[]
 
 const CourseSchema: Schema =  new Schema({
     courseCode: {type: Schema.Types.String, required:true}, 
     title: {type: Schema.Types.String, required: true},
     description: {type: Schema.Types.String, required: true},
-    programArea: {type: Schema.Types.String, required: true},
-}) 
+    programArea: {type: Array<Schema.Types.String>, required: true},
+    campuses: {type: Array<Schema.Types.String>, required: true},
+}, {collection : 'Course' }) 
 
 const CourseModel = mongoose.model<ICourseModel>('Course', CourseSchema)
 

71514037ad47d80f78f096a46f3760de297f56e8
DomiVesalius <mmillion448@gmail.com>
2023-02-08 19:38:32 -0500

    Connected username change form to backend

--- frontend/app/src/components/ProfileForms/UsernameForm.tsx
+++ frontend/app/src/components/ProfileForms/UsernameForm.tsx
@@ -1,32 +1,49 @@
 import { Button, Stack, TextField } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import * as yup from 'yup';
 import { Form, FormikProvider, useFormik } from 'formik';
-
-export interface UsernameFormValues {
-    username: string;
-}
+import HTTP from '../../utils/http';
+import useSWR from 'swr';
 
 const ChangeUsernameForm = () => {
-    const [username, setUsername] = useState('Username');
-
-    const initialValues: UsernameFormValues = {
-        username: ''
+    const getUserData = async () => {
+        try {
+            const res = await HTTP.get('/users/me');
+            return res.data;
+        } catch (e) {
+            return 'Failed to get user data';
+        }
     };
 
+    const { data, error, isLoading } = useSWR('/api/users/me', getUserData);
+
     const usernameValidationSchema = yup.object({
-        username: yup.string()
+        username: yup.string().min(1, 'Username too short').required()
     });
 
     const usernameFormik = useFormik({
-        initialValues,
+        initialValues: { username: '' },
         validationSchema: usernameValidationSchema,
-        onSubmit: async (values, { setSubmitting }) => {
+        onSubmit: async (values, { setSubmitting, setFieldError }) => {
             setSubmitting(true);
+            try {
+                const res = await HTTP.patch('/users/change-username', {
+                    newUsername: values.username
+                });
+                window.location.reload();
+            } catch (e: any) {
+                setFieldError('username', 'An unknown error occurred');
+            }
 
             setSubmitting(false);
         }
     });
+
+    useEffect(() => {
+        if (!(error || isLoading)) {
+            usernameFormik.setFieldValue('username', data.username || '');
+        }
+    }, [data]);
 
     return (
         <FormikProvider value={usernameFormik}>
@@ -38,16 +55,15 @@ const ChangeUsernameForm = () => {
                         name="username"
                         onChange={usernameFormik.handleChange}
                         value={usernameFormik.values.username}
-                        helperText={
-                            usernameFormik.touched.username && usernameFormik.errors.username
-                        }
+                        // helperText={
+                        //     usernameFormik.touched.username && usernameFormik.errors.username
+                        // }
                     />
                     <Button
+                        type="submit"
                         variant="contained"
-                        size={'small'}
-                        onClick={() => {
-                            alert('Username changed.');
-                        }}
+                        size="small"
+                        disabled={usernameFormik.isSubmitting}
                     >
                         Change username
                     </Button>

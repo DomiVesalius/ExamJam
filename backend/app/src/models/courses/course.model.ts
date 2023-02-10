6025c92101d919c7a4aad2a8a3f585689e511a05
DomiVesalius <mmillion448@gmail.com>
2023-02-10 18:37:15 -0500

    Formatting

--- backend/app/src/models/courses/course.model.ts
+++ backend/app/src/models/courses/course.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model, Schema } from "mongoose";
+import mongoose, { Document, Model, Schema } from 'mongoose';
 
 export interface ICourse {
     courseCode: string;
@@ -13,14 +13,17 @@ export interface ICourseModel extends ICourse, Document {}
 // programArea: string[]
 // campus: string[]
 
-const CourseSchema: Schema =  new Schema({
-    courseCode: {type: Schema.Types.String, required:true}, 
-    title: {type: Schema.Types.String, required: true},
-    description: {type: Schema.Types.String, required: true},
-    programArea: {type: Array<Schema.Types.String>, required: true},
-    campuses: {type: Array<Schema.Types.String>, required: true},
-}, {collection : 'Course' }) 
+const CourseSchema: Schema = new Schema(
+    {
+        courseCode: { type: Schema.Types.String, required: true },
+        title: { type: Schema.Types.String, required: true },
+        description: { type: Schema.Types.String, required: true },
+        programArea: { type: Array<Schema.Types.String>, required: true },
+        campuses: { type: Array<Schema.Types.String>, required: true }
+    },
+    { collection: 'Course' }
+);
 
-const CourseModel = mongoose.model<ICourseModel>('Course', CourseSchema)
+const CourseModel = mongoose.model<ICourseModel>('Course', CourseSchema);
 
-export default CourseModel
+export default CourseModel;
--- backend/app/src/models/exams/exam.model.ts
+++ backend/app/src/models/exams/exam.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model, Schema } from "mongoose";
+import mongoose, { Document, Model, Schema } from 'mongoose';
 
 export interface IExam {
     courseCode: string;
@@ -9,14 +9,16 @@ export interface IExam {
 
 export interface IExamModel extends IExam, Document {}
 
-const ExamSchema: Schema =  new Schema({
-    courseCode: {type: Schema.Types.String, required:true}, 
-    title: {type: Schema.Types.String, required: true},
-    link: {type: Schema.Types.String, required: true},
-    files_id: {type: Schema.Types.ObjectId, required: true},
-    
-}, {collection : 'Exam' }) 
+const ExamSchema: Schema = new Schema(
+    {
+        courseCode: { type: Schema.Types.String, required: true },
+        title: { type: Schema.Types.String, required: true },
+        link: { type: Schema.Types.String, required: true },
+        files_id: { type: Schema.Types.ObjectId, required: true }
+    },
+    { collection: 'Exam' }
+);
 
-const ExamModel = mongoose.model<IExamModel>('Exam', ExamSchema)
+const ExamModel = mongoose.model<IExamModel>('Exam', ExamSchema);
 
-export default ExamModel
+export default ExamModel;

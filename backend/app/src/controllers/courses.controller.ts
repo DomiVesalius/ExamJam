021417d8ab35d03f07d3f1af6b52aa8c518870b3
Eric Ryu <ericryu19@gmail.com>
2023-02-12 18:35:49 -0500

    add params and rename endpoint

--- backend/app/src/controllers/courses.controller.ts
+++ backend/app/src/controllers/courses.controller.ts
@@ -1,20 +1,23 @@
 import { BaseController } from './base.controller';
 import { Get, Query, Route, Tags, Request, Post, Body } from 'tsoa';
-import { Request as ExpressRequest } from'express';
+import { Request as ExpressRequest } from 'express';
 
 interface GetCoursesResponse {
-    courseResults: { courseCode: string, courseName: string }[];
+    courseResults: { courseCode: string; courseName: string }[];
 }
 
 @Tags('Courses')
-@Route('course')
+@Route('courses')
 export class CoursesController extends BaseController {
-    @Get('courses')
-    public async getCourses(): Promise<GetCoursesResponse> {
+    @Get('')
+    public async getCourses(
+        @Query() limit: number,
+        @Query() page: number
+    ): Promise<GetCoursesResponse> {
         const csc148 = {
-            courseCode: "CSC148H5",
-            courseName: "Intro to CS",
-        }
-        return { courseResults: [csc148] }
+            courseCode: 'CSC148H5',
+            courseName: 'Intro to CS'
+        };
+        return { courseResults: [csc148] };
     }
 }

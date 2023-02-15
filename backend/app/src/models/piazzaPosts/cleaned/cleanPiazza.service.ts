96e37bfc14dcbaef9f7ad755efc29018536f7deb
DomiVesalius <mmillion448@gmail.com>
2023-02-14 21:34:08 -0500

    Created MongoDB models for cleaned version of piazza posts and piazza comments. Also created service for creating such documents

---
+++ backend/app/src/models/piazzaPosts/cleaned/cleanPiazza.service.ts
@@ -0,0 +1,49 @@
+import PiazzaPostModel, { IPiazzaPostModel } from './piazzaPost.model';
+import PiazzaCommentModel, { CommentType, IPiazzaCommentModel } from './piazzaComment.model';
+import mongoose, { Schema, Types } from 'mongoose';
+
+interface CreatePiazzaPostFields {
+    _id: string;
+    courseCode: string;
+    forumId: string;
+    postNumber: number;
+    title: string;
+    content: string;
+    createdAt: string;
+}
+
+export interface CreateCommentFields {
+    _id: string;
+    postId: Schema.Types.String;
+    type: CommentType;
+    parentId: Schema.Types.String | string | null;
+    content: string;
+}
+
+export class CleanPiazzaService {
+    public static async createPiazzaPost(
+        fields: CreatePiazzaPostFields
+    ): Promise<IPiazzaPostModel | null> {
+        try {
+            return await PiazzaPostModel.create({
+                ...fields,
+                createdAt: new Date(fields.createdAt),
+                comments: []
+            });
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+    }
+
+    public static async createComment(
+        fields: CreateCommentFields
+    ): Promise<IPiazzaCommentModel | null> {
+        try {
+            return await PiazzaCommentModel.create({ ...fields, children: [] });
+        } catch (e) {
+            console.log(e);
+            return null;
+        }
+    }
+}
---
+++ backend/app/src/models/piazzaPosts/cleaned/piazzaPost.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Model, Schema, Document } from 'mongoose';
+import { PiazzaCommentModelName } from './piazzaComment.model';
+
+export const PiazzaPostModelName = 'PiazzaPost';
+
+export interface IPiazzaPostModel extends Document {
+    _id: Schema.Types.String;
+    courseCode: Schema.Types.String;
+    forumId: Schema.Types.String;
+    postNumber: Schema.Types.Number;
+    title: Schema.Types.String;
+    content: Schema.Types.String;
+    createdAt: Schema.Types.Date;
+    comments: Schema.Types.ObjectId[];
+}
+
+const PiazzaPostSchema = new Schema<IPiazzaPostModel>(
+    {
+        _id: { type: Schema.Types.String, required: true, unique: true },
+        courseCode: { type: Schema.Types.String, required: true },
+        forumId: { type: Schema.Types.String, required: true },
+        postNumber: { type: Schema.Types.Number, required: true },
+        title: { type: Schema.Types.String, required: true },
+        content: { type: Schema.Types.String, required: false, default: '' }, // required is false because some posts have an empty body
+        createdAt: { type: Schema.Types.Date, required: true },
+        comments: [{ type: Schema.Types.ObjectId, ref: `${PiazzaCommentModelName}._id` }]
+    },
+    { timestamps: false, versionKey: false, _id: false }
+);
+
+const PiazzaPostModel = mongoose.model<IPiazzaPostModel>(PiazzaPostModelName, PiazzaPostSchema);
+
+export default PiazzaPostModel;

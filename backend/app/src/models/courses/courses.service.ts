5387b60d52c6df02e56f6813c55eb77028f072ba
Eric Ryu <ericryu19@gmail.com>
2023-02-20 14:14:55 -0500

    fix regex so that search results work as intended - regex fixed to search for only courseCode or title - regex fixed to search anywhere in db's string - sort db results by courseCode first

--- backend/app/src/models/courses/courses.service.ts
+++ backend/app/src/models/courses/courses.service.ts
@@ -8,14 +8,9 @@ export class CoursesService {
         keyword: string
     ): Promise<ICourseModel[]> {
         return CourseModel.find({
-            // TODO: fix regex... not working as intended
-            $or: [
-                { title: new RegExp('^' + keyword + '$', 'i') },
-                { courseCode: new RegExp('^' + keyword + '$', 'i') },
-                { description: new RegExp('^' + keyword + '$', 'i') }
-            ]
+            $or: [{ title: new RegExp(keyword, 'i') }, { courseCode: new RegExp(keyword, 'i') }]
         })
-            .sort({ title: 'asc', courseCode: 'asc', description: 'asc' })
+            .sort({ courseCode: 'asc', title: 'asc' })
             .skip((pageNumber - 1) * limit)
             .limit(limit);
     }

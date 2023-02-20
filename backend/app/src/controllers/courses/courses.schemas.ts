fcdea8c6305ed6fa6e40ed138e6a9484b574df86
Eric Ryu <ericryu19@gmail.com>
2023-02-20 15:52:16 -0500

    add comments indicating representation of the keys

--- backend/app/src/controllers/courses/courses.schemas.ts
+++ backend/app/src/controllers/courses/courses.schemas.ts
@@ -2,5 +2,24 @@ import { BaseResponse } from '../base.controller';
 import { ICourseModel } from '../../models/courses/course.model';
 
 export interface GetCoursesResponse extends BaseResponse {
+    /**
+     * Array of courses from the database
+     */
     data: ICourseModel[];
+
+    /**
+     * Page number of courses returned
+     */
+    page: number;
+
+    /**
+     * Limit from request
+     */
+    limit: number;
+
+    /**
+     * Total number of pages as per matching results and limit
+     * Calculated as: total number of matching results / limit
+     */
+    totalPages: number;
 }

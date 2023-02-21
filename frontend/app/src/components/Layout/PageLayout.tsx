5edb9bf44a4a3a7fb290c612308ad86e9b128cd8
DomiVesalius <mmillion448@gmail.com>
2023-02-20 19:23:09 -0500

    Added Navbar component to page layout so that it is present in all pages

--- frontend/app/src/components/Layout/PageLayout.tsx
+++ frontend/app/src/components/Layout/PageLayout.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { Navbar } from '../Navbar/Navbar';
+import { Container, Grid, Stack } from '@mui/material';
 
 export interface LayoutProps {
     children?: React.ReactNode;
@@ -14,7 +16,23 @@ const PageLayout: React.FunctionComponent<LayoutProps> = (props: LayoutProps): J
         document.title = props.title;
     }, [props.title]);
 
-    return <>{props.children}</>;
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Navbar
+                    userMenu1="Profile"
+                    userMenu2="Logout"
+                    navbarName="Exam Jam"
+                    searchDefaultColor="#FFFFFF"
+                    searchHoverColor="#FFFFFF"
+                    searchPlaceholder="Search for courses..."
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <Container>{props.children}</Container>
+            </Grid>
+        </Grid>
+    );
 };
 
 export default PageLayout;

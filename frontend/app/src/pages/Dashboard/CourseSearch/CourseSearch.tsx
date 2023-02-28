0398dad4ecc903b8804125c56b2eb1c252fe741a
Eric Ryu <ericryu19@gmail.com>
2023-02-28 17:00:03 -0500

    implement handling function for keyboard press

--- frontend/app/src/pages/Dashboard/CourseSearch/CourseSearch.tsx
+++ frontend/app/src/pages/Dashboard/CourseSearch/CourseSearch.tsx
@@ -1,36 +1,39 @@
-import React, { KeyboardEventHandler } from 'react';
+import React, { KeyboardEventHandler, MouseEventHandler, useState } from 'react';
 import { CourseList } from '../../../components/CourseList/CourseList';
 import { SearchBar } from '../../../components/SearchBar/SearchBar';
 import PageLayout from '../../../components/Layout/PageLayout';
 import { blue, lightBlue } from '@mui/material/colors';
 import { Box, Container, Grid } from '@mui/material';
 const CourseSearch: React.FunctionComponent = () => {
+    const [keyword, setKeyword] = useState('');
+
     const handleKeyPress: KeyboardEventHandler<HTMLDivElement> = (e) => {
         if (e.key == 'Enter') {
-            console.log('Entered with value: ' + (e.target as HTMLInputElement).value);
+            console.log(('Entered with value: ' + (e.target as HTMLInputElement).value) as string);
+            setKeyword((e.target as HTMLInputElement).value as string);
         }
     };
-
-    const handleButtonPress = null;
 
     return (
         <PageLayout title="Search Results">
             <Box sx={{ mb: 8 }}>
-                <SearchBar
-                    hoverColor={blue['300']}
-                    defaultColor={lightBlue['300']}
-                    placeHolder="Search for courses..."
-                    handleKeyPress={handleKeyPress}
-                />
+                <Container>
+                    <SearchBar
+                        hoverColor={blue['300']}
+                        defaultColor={lightBlue['300']}
+                        placeHolder="Search for courses..."
+                        handleKeyPress={handleKeyPress}
+                    />
+                </Container>
             </Box>
             <Box>
                 <Container>
                     <CourseList
                         rowSpacing={2}
                         colSpacing={2}
-                        queryLimit={5}
+                        queryLimit={6}
                         queryPage={1}
-                        queryKeyword="csc"
+                        queryKeyword={keyword}
                     />
                 </Container>
             </Box>

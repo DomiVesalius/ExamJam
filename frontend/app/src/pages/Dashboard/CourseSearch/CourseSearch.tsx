25cfd579aeac9bd46fdd9c0fe85d069f2163a766
Eric Ryu <ericryu19@gmail.com>
2023-02-28 17:42:33 -0500

    should be protected route and use state for handling submit

--- frontend/app/src/pages/Dashboard/CourseSearch/CourseSearch.tsx
+++ frontend/app/src/pages/Dashboard/CourseSearch/CourseSearch.tsx
@@ -4,40 +4,34 @@ import { SearchBar } from '../../../components/SearchBar/SearchBar';
 import PageLayout from '../../../components/Layout/PageLayout';
 import { blue, lightBlue } from '@mui/material/colors';
 import { Box, Container, Grid } from '@mui/material';
+import ProtectedRoute from '../../../components/Routes/ProtectedRoute';
 const CourseSearch: React.FunctionComponent = () => {
     const [keyword, setKeyword] = useState('');
 
-    const handleKeyPress: KeyboardEventHandler<HTMLDivElement> = (e) => {
-        if (e.key == 'Enter') {
-            console.log(('Entered with value: ' + (e.target as HTMLInputElement).value) as string);
-            setKeyword((e.target as HTMLInputElement).value as string);
-        }
-    };
-
     return (
-        <PageLayout title="Search Results">
-            <Box sx={{ mb: 8 }}>
-                <Container>
+        <ProtectedRoute>
+            <PageLayout title="Search For Courses">
+                <Box sx={{ mb: 8 }}>
                     <SearchBar
                         hoverColor={blue['300']}
                         defaultColor={lightBlue['300']}
                         placeHolder="Search for courses..."
-                        handleKeyPress={handleKeyPress}
+                        handleSubmit={setKeyword}
                     />
-                </Container>
-            </Box>
-            <Box>
-                <Container>
-                    <CourseList
-                        rowSpacing={2}
-                        colSpacing={2}
-                        queryLimit={6}
-                        queryPage={1}
-                        queryKeyword={keyword}
-                    />
-                </Container>
-            </Box>
-        </PageLayout>
+                </Box>
+                <Box>
+                    <Container>
+                        <CourseList
+                            rowSpacing={2}
+                            colSpacing={2}
+                            queryLimit={6}
+                            queryPage={1}
+                            queryKeyword={keyword}
+                        />
+                    </Container>
+                </Box>
+            </PageLayout>
+        </ProtectedRoute>
     );
 };
 

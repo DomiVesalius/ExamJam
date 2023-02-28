6876c9a5cf797feb43b77b5da889e3259df82c0a
Eric Ryu <ericryu19@gmail.com>
2023-02-28 17:41:18 -0500

    use formik instead

--- frontend/app/src/components/SearchBar/SearchBar.tsx
+++ frontend/app/src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
-import React, { KeyboardEventHandler, MouseEventHandler } from 'react';
-import { alpha, InputBase, styled, Button, Box } from '@mui/material';
+import React, { KeyboardEventHandler, MouseEventHandler, useState } from 'react';
+import { alpha, Box, TextField, InputAdornment } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { Form, FormikProvider, useFormik } from 'formik';
 
 interface SearchProps {
     /** Color of Search Bar when hovering */
@@ -12,73 +13,46 @@ interface SearchProps {
     /** Placeholder text in search */
     placeHolder: string;
 
-    /** Optional function to handle key press for search bar */
-    handleKeyPress?: KeyboardEventHandler<HTMLDivElement>;
+    /** Function to set state for 'Enter' key press for search bar */
+    handleSubmit: React.Dispatch<React.SetStateAction<string>>;
 }
 
-/** styled('div') creates custom divs with start and end tag of the constant name */
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-}));
-
-export const SearchBar = ({
-    hoverColor,
-    defaultColor,
-    placeHolder,
-    handleKeyPress
-}: SearchProps) => {
-    const StyledInputBase = styled(InputBase)(({ theme }) => ({
-        color: 'inherit',
-        '& .MuiInputBase-input': {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('md')]: {
-                width: '20ch'
-            }
+export const SearchBar = ({ hoverColor, defaultColor, placeHolder, handleSubmit }: SearchProps) => {
+    const searchFormik = useFormik({
+        initialValues: { query: '' },
+        onSubmit: async (values) => {
+            handleSubmit(values.query);
         }
-    }));
-
-    const Search = styled('div')(({ theme }) => ({
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(defaultColor ? defaultColor : theme.palette.primary.main, 0.15),
-        '&:hover': {
-            backgroundColor: alpha(hoverColor ? hoverColor : theme.palette.primary.main, 0.25)
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '60%'
-    }));
+    });
 
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Search>
-                <SearchIconWrapper>
-                    <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase placeholder={placeHolder} onKeyPress={handleKeyPress} />
-            </Search>
-            <div onClick={() => handleKeyPress}>
-                <Button
-                    variant="outlined"
-                    size="medium"
-                    sx={{
-                        ml: -1
-                    }}
-                >
-                    Search
-                </Button>
-            </div>
+        <Box sx={{ justifyContent: 'center', width: '100%' }}>
+            <FormikProvider value={searchFormik}>
+                <Form>
+                    <TextField
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon />
+                                </InputAdornment>
+                            )
+                        }}
+                        id="query"
+                        name="query"
+                        label="Search"
+                        placeholder={placeHolder}
+                        value={searchFormik.values.query}
+                        onChange={searchFormik.handleChange}
+                        fullWidth
+                        sx={{
+                            backgroundColor: alpha(defaultColor, 0.15),
+                            '&:hover': {
+                                backgroundColor: alpha(hoverColor, 0.25)
+                            }
+                        }}
+                    />
+                </Form>
+            </FormikProvider>
         </Box>
     );
 };

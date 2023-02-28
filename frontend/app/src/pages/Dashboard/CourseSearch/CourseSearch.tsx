b1ef63cccceebe28e9cdeb11efb35871efd98c0c
Eric Ryu <ericryu19@gmail.com>
2023-02-28 15:11:44 -0500

    implement the static format for search results page

--- frontend/app/src/pages/Dashboard/CourseSearch/CourseSearch.tsx
+++ frontend/app/src/pages/Dashboard/CourseSearch/CourseSearch.tsx
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { KeyboardEventHandler } from 'react';
+import { CourseList } from '../../../components/CourseList/CourseList';
+import { SearchBar } from '../../../components/SearchBar/SearchBar';
+import PageLayout from '../../../components/Layout/PageLayout';
+import { blue, lightBlue } from '@mui/material/colors';
+import { Box, Container, Grid } from '@mui/material';
 const CourseSearch: React.FunctionComponent = () => {
-    return null;
-}
+    const handleKeyPress: KeyboardEventHandler<HTMLDivElement> = (e) => {
+        if (e.key == 'Enter') {
+            console.log('Entered with value: ' + (e.target as HTMLInputElement).value);
+        }
+    };
+
+    const handleButtonPress = null;
+
+    return (
+        <PageLayout title="Search Results">
+            <Box sx={{ mb: 8 }}>
+                <SearchBar
+                    hoverColor={blue['300']}
+                    defaultColor={lightBlue['300']}
+                    placeHolder="Search for courses..."
+                    handleKeyPress={handleKeyPress}
+                />
+            </Box>
+            <Box>
+                <Container>
+                    <CourseList
+                        rowSpacing={2}
+                        colSpacing={2}
+                        queryLimit={5}
+                        queryPage={1}
+                        queryKeyword="csc"
+                    />
+                </Container>
+            </Box>
+        </PageLayout>
+    );
+};
 
 export default CourseSearch;

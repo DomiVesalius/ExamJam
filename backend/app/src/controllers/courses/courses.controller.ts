72d3d794739f18cf33c0b9ba5e0a6f95a3421e39
DomiVesalius <mmillion448@gmail.com>
2023-03-02 20:39:14 -0500

    Fixed bug where status code would be 200 even when no exams were found

--- backend/app/src/controllers/courses/courses.controller.ts
+++ backend/app/src/controllers/courses/courses.controller.ts
@@ -1,33 +1,27 @@
-import { BaseController, } from '../base.controller';
+import { BaseController } from '../base.controller';
 import { Get, Route, Tags, Path } from 'tsoa';
 import { CourseService } from '../../models/courses/courses.service';
-import { CourseResponse, GetExamsResponse } from './courses.schema';
+import { CourseResponse, GetExamsResponse } from './courses.schemas';
 import { ExamService } from '../../models/exams/exam.service';
 
 @Tags('Courses')
 @Route('courses')
 export class CoursesController extends BaseController {
-    @Get("{courseCode}")
-    public async getCourse(
-        @Path() courseCode: string
-    ): Promise<CourseResponse> {
-        const course = await CourseService.getByCourseId(courseCode)
+    @Get('{courseCode}')
+    public async getCourse(@Path() courseCode: string): Promise<CourseResponse> {
+        const course = await CourseService.getByCourseId(courseCode);
         const code = course ? 200 : 404;
-        this.setStatus(code)
-        const success = course ? true : false;
+        this.setStatus(code);
+        const success = !!course;
         return { data: course, success: success, code: code };
     }
 
-    @Get("{courseCode}/exams")
-    public async getExams(
-        @Path() courseCode: string
-    ): Promise<GetExamsResponse> {
-        const exams = await ExamService.getByCourseId(courseCode)
-        const code = exams ? 200 : 404;
-        this.setStatus(code)
-        const success = exams ? true : false;
+    @Get('{courseCode}/exams')
+    public async getExams(@Path() courseCode: string): Promise<GetExamsResponse> {
+        const exams = await ExamService.getByCourseId(courseCode);
+        const code = exams.length ? 200 : 404;
+        this.setStatus(code);
+        const success = !!exams.length;
         return { data: exams, success: success, code: code };
     }
 }
-
-

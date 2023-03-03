fad0be76a91450edd64d93f8571e450dbec05619
Ananth Chebolu <ananth.chebolu@gmail.com>
2023-03-03 18:57:58 -0500

    Finsihed making the PDF Viewer Story

--- frontend/app/src/components/Exam/Exam.tsx
+++ frontend/app/src/components/Exam/Exam.tsx
@@ -9,7 +9,7 @@ interface ExamProps {
 const Exam: React.FunctionComponent<ExamProps> = (props: ExamProps) => {
     return (
         <>
-            <PDFViewer pdf={`/api/exams/files/${props.examId}`}/>
+            <PDFViewer pdf={`/api/exams/files/${props.examId}`} width={"60vw"} height={"90vh"}/>
         </>
     );
 };
--- frontend/app/src/components/Exam/PDFViewer/PDFViewer.tsx
+++ frontend/app/src/components/Exam/PDFViewer/PDFViewer.tsx
@@ -7,13 +7,15 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 
 interface PDFViewerProps {
     pdf: string;
+    width: string;
+    height: string;
 }
 
 const PDFViewer: React.FunctionComponent<PDFViewerProps> = (props: PDFViewerProps) => {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
     return (
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.min.js">
-            <div style={{ border: '1px solid rgba(0, 0, 0, 0.3)'}}>
+            <div style={{ border: '1px solid rgba(0, 0, 0, 0.3)', width: props.width, height: props.height, marginBottom:"10vh"}}>
                 <Viewer fileUrl={props.pdf} plugins={[defaultLayoutPluginInstance]} />
             </div>
         </Worker>

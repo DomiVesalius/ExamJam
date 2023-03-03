f5d8c0e2d3080edf1398af4caa4e07da6c12abac
DomiVesalius <mmillion448@gmail.com>
2023-03-02 22:45:38 -0500

    Created PiazzaPostPreview component that displays title, content, and other data of a piazza post

---
+++ frontend/app/src/components/Piazza/PiazzaPostPreview/PiazzaPostPreview.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Button, Card, CardActionArea, CardContent, CardHeader, Typography } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import ShareIcon from '@mui/icons-material/Share';
+import { useNavigate } from 'react-router-dom';
+
+export interface PiazzaPostPreviewProps {
+    postId: string;
+    forumId: string;
+    postNumber: number;
+    courseCode: string;
+    title: string;
+    content: string;
+    createdAt: Date;
+    numComments: number;
+    previewTextMaxLength: number;
+}
+
+// TODO: Resource https://stackoverflow.com/questions/23616226/insert-html-with-react-variable-statements-jsx
+// <div dangerouslySetInnerHTML={{ __html: previewText }}></div>
+
+const PiazzaPostPreview: React.FunctionComponent<PiazzaPostPreviewProps> = (
+    props: PiazzaPostPreviewProps
+) => {
+    const navigate = useNavigate();
+    const parsedHTML = new DOMParser().parseFromString(props.content, 'text/html');
+    const textContent = parsedHTML.body.textContent || '';
+    const previewText = textContent.substring(
+        0,
+        Math.min(textContent.length, props.previewTextMaxLength)
+    ); // 200 could me modified to be any arbitrary number
+
+    const handleNavigateToPost = () =>
+        navigate(`forums/${props.forumId}/posts/${props.postNumber}`);
+
+    return (
+        <Card sx={{ width: '50vw' }}>
+            <CardActionArea onClick={handleNavigateToPost}>
+                <CardContent>
+                    <Typography color="text.secondary">
+                        Sourced from forum {props.forumId}. Post # {props.postNumber}
+                    </Typography>
+                    <CardHeader
+                        title={`${props.title}`}
+                        subheader={props.createdAt.toDateString()}
+                        sx={{ fontWeight: 'bold' }}
+                    />
+                    <Typography variant="body1">{previewText}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActionArea sx={{ display: 'flex', justifyContent: 'right' }}>
+                <Button size="small" color="primary" onClick={handleNavigateToPost}>
+                    {props.numComments} Comments
+                </Button>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+            </CardActionArea>
+        </Card>
+    );
+};
+
+export default PiazzaPostPreview;

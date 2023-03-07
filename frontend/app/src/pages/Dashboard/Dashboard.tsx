0e654665b4461ff848a2f2abf842e7aa91c0e47b
Eric Ryu <ericryu19@gmail.com>
2023-03-07 14:20:49 -0500

    add bookmarked courses heading

--- frontend/app/src/pages/Dashboard/Dashboard.tsx
+++ frontend/app/src/pages/Dashboard/Dashboard.tsx
@@ -3,11 +3,16 @@ import ProtectedRoute from '../../components/Routes/ProtectedRoute';
 import PageLayout from '../../components/Layout/PageLayout';
 import { CourseList, endpointTypes } from '../../components/CourseList/CourseList';
 import { bookmarkTypes } from '../../utils/bookmarkConstants';
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
 
 const Dashboard: React.FunctionComponent = () => {
     return (
         <ProtectedRoute>
             <PageLayout title="Dashboard">
+                <Box>
+                    <Typography variant="h2">Bookmarked Courses</Typography>
+                </Box>
                 <CourseList
                     rowSpacing={2}
                     colSpacing={2}

6ffd185c7858567b795ade8ff2c2dd7cc1795fed
Eric Ryu <ericryu19@gmail.com>
2023-03-13 15:34:50 -0400

    domi will create comments controller

--- backend/app/src/controllers/comments/comments.schemas.ts
+++
@@ -1,8 +0,0 @@
-import { BaseResponse } from '../base.controller';
-
-export interface GetCommentsResponse extends BaseResponse {
-    data: string;
-    page: number;
-    limit: number;
-    totalPages: number;
-}

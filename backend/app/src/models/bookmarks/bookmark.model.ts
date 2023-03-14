90fbe5633d8d97a1e69a60720cae3cd4e4c7397c
DomiVesalius <mmillion448@gmail.com>
2023-03-13 20:55:17 -0400

    Changed userId to userEmail since email is already unique

--- backend/app/src/models/bookmarks/bookmark.model.ts
+++ backend/app/src/models/bookmarks/bookmark.model.ts
@@ -1,11 +1,11 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IBookmark {
-    userId: string;
+    userEmail: string;
     type: string;
     courseCode: string;
     examId: string;
-    postId: string
+    postId: string;
 }
 
 export enum BookmarkType {
@@ -18,15 +18,14 @@ export interface IBookmarkModel extends IBookmark, Document {}
 
 // courseCode, examId, postId need to be populated depending on what is being saved
 // not all 3 should be populated when creating a new bookmark.
-const BookmarkSchema: Schema = new Schema(
-    {
-        userId: {type: Schema.Types.ObjectId, required: true, ref: 'User._id'},
-        type: {type: Schema.Types.String, enum: BookmarkType, required: true},
-        courseCode: {type: Schema.Types.String, ref: 'Course.courseCode'},
-        examId: {type: Schema.Types.ObjectId, ref: 'Exam._id'},
-        postId: {type: Schema.Types.ObjectId, ref: 'Post._id'},
-    }
-);
+const BookmarkSchema: Schema = new Schema({
+    userEmail: { type: Schema.Types.ObjectId, required: true, ref: 'User.email' },
+    type: { type: Schema.Types.String, enum: BookmarkType, required: true },
+    courseCode: { type: Schema.Types.String, ref: 'Course.courseCode' },
+    examId: { type: Schema.Types.ObjectId, ref: 'Exam._id' },
+    postId: { type: Schema.Types.ObjectId, ref: 'Post._id' }
+});
+
 const BookmarkModel = mongoose.model<IBookmarkModel>('Bookmark', BookmarkSchema);
 
 export default BookmarkModel;

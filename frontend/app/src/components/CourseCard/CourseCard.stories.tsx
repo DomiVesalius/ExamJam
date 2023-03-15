5a65735b75897e2c920b6ffb33f4567d6538d8c5
abhinavmeda <39966275+abhinavmeda@users.noreply.github.com>
2023-03-15 19:24:27 -0400

    added storybook

--- frontend/app/src/components/CourseCard/CourseCard.stories.tsx
+++ frontend/app/src/components/CourseCard/CourseCard.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { CourseCard } from './CourseCard';
 
 export default {
-    title: 'CourseCard',
+    title: `Components/${CourseCard.name}`,
     component: CourseCard,
     parameters: {
         layout: 'centered'
@@ -13,13 +13,28 @@ export default {
 
 const Template: ComponentStory<typeof CourseCard> = (args) => <CourseCard {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Unbookmarked = Template.bind({});
+Unbookmarked.args = {
     mainText: 'Example Card (Main Text)',
     bodyText: 'This is the body text of the example card.',
     imgPath: 'https://source.unsplash.com/random/',
     imgAlt: 'Random Unsplash Image',
     width: 345,
     height: 140,
-    redirectURL: '#'
+    redirectURL: '#',
+    isBookmarked: false,
 };
+
+export const Bookmarked = Template.bind({});
+Bookmarked.args = {
+    mainText: 'Example Card (Main Text)',
+    bodyText: 'This is the body text of the example card.',
+    imgPath: 'https://source.unsplash.com/random/',
+    imgAlt: 'Random Unsplash Image',
+    width: 345,
+    height: 140,
+    redirectURL: '#',
+    isBookmarked: true,
+};
+
+

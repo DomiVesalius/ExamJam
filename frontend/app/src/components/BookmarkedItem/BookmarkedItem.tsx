547db28a80a5d3459308a266b905fda87a5860f6
Eric Ryu <ericryu19@gmail.com>
2023-03-15 16:11:15 -0400

    set template for setting states and fetching from backend

--- frontend/app/src/components/BookmarkedItem/BookmarkedItem.tsx
+++ frontend/app/src/components/BookmarkedItem/BookmarkedItem.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetcher } from '../../utils/helpers';
+import useSWR from 'swr';
+import { Box } from '@mui/material';
 
 export enum BookmarkedItemType {
     post = 'post',
@@ -11,10 +14,58 @@ export interface BookmarkedItemProps {
     queryLimit: number;
 }
 
+function createBookmarkedElements(
+    data: any,
+    type: BookmarkedItemType
+): [React.ReactElement[], number] {
+    const bookmarkedElements: React.ReactElement[] = [];
+    for (let bookmarkedItem of data.data) {
+        if (type == BookmarkedItemType.exam) {
+            // TODO: fetch data.data of type IExamModel[]
+        } else if (type == BookmarkedItemType.post) {
+            // TODO: fetch data.data of type IPostModel[]
+        }
+    }
+    return [bookmarkedElements, data.totalPages];
+}
+
 const BookmarkedItem: React.FunctionComponent<BookmarkedItemProps> = (
     props: BookmarkedItemProps
 ) => {
-    return <div></div>;
+    let endpoint: string = '';
+    const [page, setPage] = React.useState(props.queryPage);
+    if (props.type == BookmarkedItemType.exam) {
+        // bookmarked exam
+        endpoint = `/bookmarks/${BookmarkedItemType.exam}?limit=${props.queryLimit}&page=${props.queryPage}`;
+    } else if (props.type == BookmarkedItemType.post) {
+        // bookmarked post
+        endpoint = `/bookmarks/${BookmarkedItemType.post}?limit=${props.queryLimit}&page=${props.queryPage}`;
+    }
+
+    if (endpoint == '') {
+        return <div></div>;
+    }
+
+    const { data, error } = useSWR(endpoint, fetcher);
+    const [totalPages, setTotalPages] = React.useState(1);
+    const [bookmarkedItems, setBookmarkedItems] = React.useState<React.ReactElement[]>([]);
+
+    useEffect(() => {
+        if (data) {
+            const bookmarkedElements = createBookmarkedElements(data, props.type);
+            setBookmarkedItems(bookmarkedElements[0]);
+            setTotalPages(bookmarkedElements[1]);
+        }
+        if (error) {
+            console.log(error);
+        }
+    }, [data, error]);
+
+    const handleChangePage = (event: React.ChangeEvent<unknown>, newPage: number) => {
+        setPage(newPage);
+    };
+
+    return <Box></Box>;
 };
 
 export default BookmarkedItem;

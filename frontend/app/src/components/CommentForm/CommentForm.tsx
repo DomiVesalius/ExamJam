9fc921dd9fa1b56a72f18e1698ad4b806fcd63cd
Fernando Sousa <fernandomancinidesousa@gmail.com>
2023-03-16 01:05:44 -0400

    Changed Form to rich text editor

--- frontend/app/src/components/CommentForm/CommentForm.tsx
+++ frontend/app/src/components/CommentForm/CommentForm.tsx
@@ -1,36 +1,34 @@
-import { Avatar, Box, Button,Card, Stack, TextField} from '@mui/material';
-import { useState } from 'react';
+import { Avatar, Box, Button, Card, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
 
 function CommentForm() {
-    const [commentTxt, setCommentTxt] = useState("");
-    const imgLink = "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+    const [commentTxt, setCommentTxt] = useState('');
+    const imgLink =
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
+    const [richTextValue, setRichTextValue] = useState('');
 
     return (
         <Card>
-            <Box sx={{ p: "15px" }}>
+            <Box sx={{ p: '15px' }}>
                 <Stack direction="row" spacing={2} alignItems="flex-start">
-                    <Avatar
-                        src={imgLink}
-                        variant="rounded"
-                        alt="user avatar"
-                    />
-                    <TextField
-                        multiline
-                        fullWidth
-                        minRows={4}
-                        id="outlined-multilined"
-                        placeholder="Add a comment"
-                        value={commentTxt}
-                        onChange={(e) => {
-                            setCommentTxt(e.target.value);
+                    <Avatar src={imgLink} variant="rounded" alt="user avatar" />
+                    <ReactQuill
+                        theme="snow"
+                        placeholder="What is your question?"
+                        value={richTextValue}
+                        style={{
+                            width: '100%',
+                            height: 'auto'
                         }}
+                        onChange={setRichTextValue}
                     />
                     <Button
                         size="large"
                         variant="contained"
                         type="submit"
                         sx={{
-                            p: "8px 25px"
+                            p: '8px 25px'
                         }}
                     >
                         Send

387bc9030cba216a7de8754a9feeb487ee93a6ee
DomiVesalius <mmillion448@gmail.com>
2023-03-17 02:40:16 -0400

    Formatting

--- frontend/app/src/components/BookmarkButton/BookmarkButton.tsx
+++ frontend/app/src/components/BookmarkButton/BookmarkButton.tsx
@@ -3,41 +3,36 @@ import BookmarkIcon from '@material-ui/icons/Bookmark';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import { BookmarkType } from '../../utils/helpers';
 import { IconButton } from '@mui/material';
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 interface BookmarkProps {
-
     type: BookmarkType;
-    
-    itemId: string
 
-    isBookmarked: boolean
+    itemId: string;
+
+    isBookmarked: boolean;
 }
 
-const BookmarkButton:React.FunctionComponent<BookmarkProps> = ({ type, itemId, isBookmarked }) => {
-    const [bookmarkState, setBookmarkState] = useState(isBookmarked)
-    
+const BookmarkButton: React.FunctionComponent<BookmarkProps> = ({ type, itemId, isBookmarked }) => {
+    const [bookmarkState, setBookmarkState] = useState(isBookmarked);
+
     const handleBookmark = async () => {
-        try{
-           const result = await HTTP.post('/bookmarks', {type, itemId})
-            if(result.status < 400) setBookmarkState(!bookmarkState)
+        try {
+            const result = await HTTP.post('/bookmarks', { type, itemId });
+            setBookmarkState(!!result.data.data);
+        } catch (e) {
+            console.log(e);
         }
-        catch(e){
-            console.log(e)
-        }
-    }
+    };
+
     let icon;
-    if(bookmarkState){
-        icon = <BookmarkIcon/>
+    if (bookmarkState) {
+        icon = <BookmarkIcon />;
+    } else {
+        icon = <BookmarkBorderIcon />;
     }
-    else{
-        icon = <BookmarkBorderIcon/>
-    }
-    return (
-        <IconButton onClick={handleBookmark}>
-           {icon}
-        </IconButton>
-    )
 
-}
+    return <IconButton onClick={handleBookmark}>{icon}</IconButton>;
+};
 
-export default BookmarkButton
+export default BookmarkButton;

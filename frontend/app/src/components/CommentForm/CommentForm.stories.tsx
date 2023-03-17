7c8dc8e89f865817c85ed28ed2cbb4f689bf6c0f
Fernando Sousa <fernandomancinidesousa@gmail.com>
2023-03-16 22:20:59 -0400

    Created Name Avatar component and created requests for adding comments through form

--- frontend/app/src/components/CommentForm/CommentForm.stories.tsx
+++ frontend/app/src/components/CommentForm/CommentForm.stories.tsx
@@ -6,16 +6,21 @@ import CommentForm from './CommentForm';
 
 // This is metadata about the component that will be used to generate the docs tab
 export default {
-    title: `CommentForm`,
+    title: `Components/${CommentForm.name}`,
     component: CommentForm
 } as ComponentMeta<typeof CommentForm>;
 
-const Template: ComponentStory<typeof CommentForm> = () => {
+const Template: ComponentStory<typeof CommentForm> = (args) => {
     return (
         <Provider store={store}>
-            <CommentForm />
+            <CommentForm {...args} />
         </Provider>
     );
 };
 
 export const General = Template.bind({});
+General.args = {
+    parentId: null,
+    postId: '6413938ca9fb562a9feb94fa',
+    author: 'john.doe@mail.utoronto.ca'
+};
--- frontend/app/src/components/CommentForm/CommentForm.tsx
+++ frontend/app/src/components/CommentForm/CommentForm.tsx
@@ -1,42 +1,82 @@
-import { Avatar, Box, Button, Card, Stack, TextField } from '@mui/material';
+import { Button, Card, Stack, CardContent } from '@mui/material';
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
+import { useFormik, Form, FormikProvider } from 'formik';
+import HTTP from '../../utils/http';
+import NameAvatar from '../NameAvatar/NameAvatar';
 
-function CommentForm() {
-    const [commentTxt, setCommentTxt] = useState('');
-    const imgLink =
-        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
+interface CommentFormProps {
+    postId: string;
+    parentId: string | null;
+    author: string;
+}
+
+const CommentForm: React.FunctionComponent<CommentFormProps> = (props: CommentFormProps) => {
     const [richTextValue, setRichTextValue] = useState('');
 
+    const initialValues = {
+        content: richTextValue
+    };
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit: async (values, { setSubmitting, setFieldError }) => {
+            setSubmitting(true);
+
+            const commentCreationValues = {
+                content: richTextValue,
+                postId: props.postId,
+                parentId: props.parentId
+            };
+
+            try {
+                const result = await HTTP.post('/comments', commentCreationValues);
+                window.location.reload();
+            } catch (e) {
+                setFieldError('content', 'Unexpected error. Try again...');
+            }
+
+            setSubmitting(false);
+        }
+    });
+
     return (
-        <Card>
-            <Box sx={{ p: '15px' }}>
-                <Stack direction="row" spacing={2} alignItems="flex-start">
-                    <Avatar src={imgLink} variant="rounded" alt="user avatar" />
-                    <ReactQuill
-                        theme="snow"
-                        placeholder="What is your question?"
-                        value={richTextValue}
-                        style={{
-                            width: '100%',
-                            height: 'auto'
-                        }}
-                        onChange={setRichTextValue}
-                    />
-                    <Button
-                        size="large"
-                        variant="contained"
-                        type="submit"
-                        sx={{
-                            p: '8px 25px'
-                        }}
-                    >
-                        Send
-                    </Button>
-                </Stack>
-            </Box>
+        <Card sx={{ padding: '15px' }}>
+            <CardContent>
+                <FormikProvider value={formik}>
+                    <Form onSubmit={formik.handleSubmit}>
+                        <Stack direction="row" spacing={2}>
+                            <NameAvatar name={props.author} />
+                            <ReactQuill
+                                theme="snow"
+                                placeholder="What is your question?"
+                                value={richTextValue}
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                    paddingBottom: '10px'
+                                }}
+                                onChange={setRichTextValue}
+                            />
+                            <Button
+                                size="large"
+                                variant="contained"
+                                type="submit"
+                                sx={{
+                                    p: '8px 25px',
+                                    height: 'fit-content'
+                                }}
+                                disabled={formik.isSubmitting}
+                            >
+                                Send
+                            </Button>
+                        </Stack>
+                        <div>{formik.errors.content}</div>
+                    </Form>
+                </FormikProvider>
+            </CardContent>
         </Card>
     );
-}
+};
 
 export default CommentForm;

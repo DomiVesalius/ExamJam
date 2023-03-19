16c82f6e2687c5d1697691accf717d340512c3b6
Eric Ryu <ericryu19@gmail.com>
2023-03-19 16:36:29 -0400

    use suneditor instead of quill

--- frontend/app/src/components/Post/PostCreationForm/PostCreationForm.tsx
+++ frontend/app/src/components/Post/PostCreationForm/PostCreationForm.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { Form, FormikProvider, useFormik } from 'formik';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import SunEditor, { buttonList } from 'suneditor-react';
+import SunEditorCore from 'suneditor/src/lib/core';
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
+import 'suneditor/dist/css/suneditor.min.css';
 
 import {
     Button,
@@ -27,13 +32,14 @@ export interface PostCreationFormValues {
 }
 
 const PostCreationForm: React.FunctionComponent<PostCreationFormProps> = ({ onSuccess }) => {
+    const editor = useRef<SunEditorCore>();
     const routeParams = useParams();
     const courseCode = routeParams['courseCode'];
 
     const [searchParams] = useSearchParams();
     const initialExam = searchParams.get('examId') || '';
 
-    const [richTextValue, setRichTextValue] = useState('');
+    const [value, setValue] = useState('');
     const [examValue, setExamValue] = useState(initialExam);
 
     const initialValues: PostCreationFormValues = {
@@ -45,7 +51,7 @@ const PostCreationForm: React.FunctionComponent<PostCreationFormProps> = ({ onSu
         onSubmit: async (values, { setSubmitting }) => {
             setSubmitting(true);
             const postCreationValues = {
-                content: richTextValue,
+                content: value,
                 title: values.title,
                 examId: examValue
             };
@@ -58,6 +64,25 @@ const PostCreationForm: React.FunctionComponent<PostCreationFormProps> = ({ onSu
     });
     if (!courseCode) {
         return <div>ERROR</div>;
+    }
+
+    const getSunEditorInstance = (sunEditor: SunEditorCore) => {
+        editor.current = sunEditor;
+    };
+
+    function handleChange(content: string) {
+        setValue(content);
+        console.log(content);
+    }
+
+    function toggleCodeView(isCodeView: boolean) {
+        console.log(isCodeView);
+    }
+
+    const sunEditorButtonList: Array<Array<string>> = [['math']];
+    for (let button of buttonList.formatting) {
+        if (typeof button == 'string') sunEditorButtonList.push([button]);
+        else sunEditorButtonList.push(button);
     }
 
     return (
@@ -87,16 +112,19 @@ const PostCreationForm: React.FunctionComponent<PostCreationFormProps> = ({ onSu
                                             }
                                             helperText={formik.touched.title && formik.errors.title}
                                         />
-                                        <ReactQuill
-                                            theme="snow"
-                                            placeholder="What is your question?"
-                                            value={richTextValue}
-                                            style={{
-                                                maxWidth: '100%',
-                                                height: 'auto'
+                                        <SunEditor
+                                            setOptions={{
+                                                katex: katex,
+                                                buttonList: sunEditorButtonList
                                             }}
-                                            onChange={setRichTextValue}
+                                            onChange={handleChange}
+                                            toggleCodeView={toggleCodeView}
                                         />
+                                        <div
+                                            dangerouslySetInnerHTML={{
+                                                __html: '<p>​<span class="__se__katex katex" data-exp="\\mathbb R" data-font-size="1em" style="font-size: 1em;" contenteditable="false"><span class="katex-mathml"><math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><semantics><mrow><mi mathvariant="double-struck">R</mi></mrow><annotation encoding="application/x-tex">\\mathbb R</annotation></semantics></math></span><span class="katex-html" aria-hidden="true"><span class="base"><span class="strut" style="height:0.6889em;"></span><span class="mord mathbb">R</span></span></span></span>​​<br></p>'
+                                            }}
+                                        ></div>
                                     </Stack>
                                     <Button
                                         disabled={formik.isSubmitting}

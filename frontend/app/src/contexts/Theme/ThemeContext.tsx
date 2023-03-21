ecdd6c947f0596507af4d039373abfa23636952f
Ananth Chebolu <ananth.chebolu@gmail.com>
2023-03-21 14:42:57 -0400

    finished DarkMode Feature. Will fix any inconsistencies that arise in task/uiUpgrade

---
+++ frontend/app/src/contexts/Theme/ThemeContext.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+export const ThemeContext: React.FunctionComponent = () => {
+    const theme = useTheme();
+    const colorMode = React.useContext(ColorModeContext);
+    return (
+        <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+            {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+    );
+};
--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -3,16 +3,49 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import MainContextProvider from './contexts/Main/MainContextProvider';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ColorModeContext } from './contexts/Theme/ThemeContext';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+export default function ToggleColorMode() {
+    const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            }
+        }),
+        []
+    );
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode
+                }
+            }),
+        [mode]
+    );
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+}
+
 root.render(
     <Provider store={store}>
         <MainContextProvider>
-            <App />
+            <ToggleColorMode />
         </MainContextProvider>
     </Provider>
 );

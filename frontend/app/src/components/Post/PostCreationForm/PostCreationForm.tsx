3fcf9cb5adf66c26db533305bdcc56800d9f1feb
Eric Ryu <ericryu19@gmail.com>
2023-03-21 15:26:16 -0400

    use react-md-editor

--- frontend/app/src/components/Post/PostCreationForm/PostCreationForm.tsx
+++ frontend/app/src/components/Post/PostCreationForm/PostCreationForm.tsx
@@ -1,13 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { Form, FormikProvider, useFormik } from 'formik';
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import SunEditor, { buttonList } from 'suneditor-react';
-import SunEditorCore from 'suneditor/src/lib/core';
-import katex from 'katex';
+import MDEditor from '@uiw/react-md-editor';
 import 'katex/dist/katex.min.css';
-import 'suneditor/dist/css/suneditor.min.css';
 
 import {
     Button,
@@ -32,7 +28,6 @@ export interface PostCreationFormValues {
 }
 
 const PostCreationForm: React.FunctionComponent<PostCreationFormProps> = ({ onSuccess }) => {
-    const editor = useRef<SunEditorCore>();
     const routeParams = useParams();
     const courseCode = routeParams['courseCode'];
 
@@ -66,23 +61,9 @@ const PostCreationForm: React.FunctionComponent<PostCreationFormProps> = ({ onSu
         return <div>ERROR</div>;
     }
 
-    const getSunEditorInstance = (sunEditor: SunEditorCore) => {
-        editor.current = sunEditor;
-    };
-
     function handleChange(content: string) {
         setValue(content);
         console.log(content);
-    }
-
-    function toggleCodeView(isCodeView: boolean) {
-        console.log(isCodeView);
-    }
-
-    const sunEditorButtonList: Array<Array<string>> = [['math']];
-    for (let button of buttonList.formatting) {
-        if (typeof button == 'string') sunEditorButtonList.push([button]);
-        else sunEditorButtonList.push(button);
     }
 
     return (
@@ -112,19 +93,12 @@ const PostCreationForm: React.FunctionComponent<PostCreationFormProps> = ({ onSu
                                             }
                                             helperText={formik.touched.title && formik.errors.title}
                                         />
-                                        <SunEditor
-                                            setOptions={{
-                                                katex: katex,
-                                                buttonList: sunEditorButtonList
-                                            }}
-                                            onChange={handleChange}
-                                            toggleCodeView={toggleCodeView}
+                                        {/* @ts-ignore */}
+                                        <MDEditor value={value} onChange={setValue} />
+                                        <MDEditor.Markdown
+                                            source={value}
+                                            style={{ whiteSpace: 'pre-wrap' }}
                                         />
-                                        <div
-                                            dangerouslySetInnerHTML={{
-                                                __html: '<p>​<span class="__se__katex katex" data-exp="\\mathbb R" data-font-size="1em" style="font-size: 1em;" contenteditable="false"><span class="katex-mathml"><math xmlns="http://www.w3.org/1998/Math/MathML" display="block"><semantics><mrow><mi mathvariant="double-struck">R</mi></mrow><annotation encoding="application/x-tex">\\mathbb R</annotation></semantics></math></span><span class="katex-html" aria-hidden="true"><span class="base"><span class="strut" style="height:0.6889em;"></span><span class="mord mathbb">R</span></span></span></span>​​<br></p>'
-                                            }}
-                                        ></div>
                                     </Stack>
                                     <Button
                                         disabled={formik.isSubmitting}

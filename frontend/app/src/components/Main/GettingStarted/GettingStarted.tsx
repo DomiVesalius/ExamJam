1ccb3534e26c0b06d5c17559fba4abbfbf2154c3
Ananth Chebolu <ananth.chebolu@gmail.com>
2023-03-29 01:54:02 -0400

    Created a temporary Landing Page

---
+++ frontend/app/src/components/Main/GettingStarted/GettingStarted.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { CssBaseline, Typography, Button } from '@material-ui/core';
+import { Feature } from '../Feature/Feature';
+
+const useStyles = makeStyles((theme: any) => ({
+    root: {
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        height: '100vh'
+    },
+    colorText: {
+        color: '#07b0ff'
+    },
+    title: {
+        fontFamily: 'Nunito'
+    },
+    butt: {
+        borderRadius: '20px',
+        width: '150px'
+    }
+}));
+
+export const GettingStarted: React.FunctionComponent = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root} id={'GettingStarted'}>
+            <CssBaseline />
+            <div>
+                <div>
+                    <Typography variant="h4">Success in every step</Typography>
+                    <Typography variant="body2">Success in every step</Typography>
+                </div>
+                <div>
+                    <Button className={classes.butt} variant="contained">
+                        Contained
+                    </Button>
+                    <Button className={classes.butt} variant="outlined">
+                        Outlined
+                    </Button>
+                </div>
+            </div>
+            <img src={`${process.env.PUBLIC_URL + '/image3.jpg'}`} alt="Logo" />;
+        </div>
+    );
+};
--- frontend/app/src/pages/Main/MainPage.tsx
+++ frontend/app/src/pages/Main/MainPage.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import PageLayout from '../../components/Layout/PageLayout';
 import UnprotectedRoute from '../../components/Routes/UnprotectedRoute';
+import { makeStyles } from '@material-ui/styles';
+import { CssBaseline } from '@material-ui/core';
+import { Header } from '../../components/Main/Header/Header';
+import { GettingStarted } from '../../components/Main/GettingStarted/GettingStarted';
+import { Feature } from '../../components/Main/Feature/Feature';
+const useStyles = makeStyles((theme: any) => ({
+    root: {
+        minHeight: '100vh',
+        backgroundImage: `url(${process.env.PUBLIC_URL + '/image7.jpg'})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+    }
+}));
 
 export const MainPage: React.FunctionComponent = () => {
+    const classes = useStyles();
     return (
-        <UnprotectedRoute>
-            <PageLayout title="ExamPage">
-                <h1>Main Page</h1>
+        <div>
+            <CssBaseline />
+            <PageLayout title="HomePage">
+                <CssBaseline />
+                <Header />
+                <GettingStarted />
             </PageLayout>
-        </UnprotectedRoute>
+        </div>
     );
 };

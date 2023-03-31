91aee489c7ebc26974c2fcaaae2a84e37198682b
DomiVesalius <mmillion448@gmail.com>
2023-03-31 03:52:45 -0400

    Added piazza post path to utils enum

--- frontend/app/src/utils/pathConstants.ts
+++ frontend/app/src/utils/pathConstants.ts
@@ -9,7 +9,8 @@ enum PathConstants {
     examPage = '/dashboard/courses/:courseCode/exams/:examId',
     coursePage = 'dashboard/courses/:courseCode',
     postPage = '/dashboard/courses/:courseCode/posts/:postId',
-    postCreationPage = '/dashboard/courses/:courseCode/create-post'
+    postCreationPage = '/dashboard/courses/:courseCode/create-post',
+    piazzaPostPage = '/dashboard/courses/forums/:forumId/posts/:postNumber'
 }
 
 export default PathConstants;
